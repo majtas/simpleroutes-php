b2fd1695dbc146b8acc4c0823a163ce80b6c9cfd
Piotr Majkutewicz <piotrek.majkutewicz@gmail.com>
2021-01-08 23:51:38 +0100

    Added post method

--- Routes/Routes.php
+++ Routes/Routes.php
@@ -8,10 +8,18 @@ class Routes {
 
 	private static array $routes = [];
 
-		public static function get(string $uri, Closure $call) {
+	public static function get(string $uri, Closure $call) {
 		if($_SERVER['REQUEST_METHOD'] === 'GET') {
 			$uri = trim($uri, '/');
 			return (static::$routes[$uri] = $call);
 		}
 	}
+
+	public static function post(string $uri, Closure $call) {
+		if($_SERVER['REQUEST_METHOD'] === 'POST') {
+			$uri = trim($uri, '/');
+			static::$routes[$uri] = $call;
+		}
+	}
+
 }
