9cbfae9661a974e3055beb55378122f0d7483790
Piotr Majkutewicz <piotrek.majkutewicz@gmail.com>
2021-01-08 23:49:48 +0100

    Added get method

--- Routes/Routes.php
+++ Routes/Routes.php
@@ -2,6 +2,16 @@
 
 namespace Majtas\Routes;
 
+use Closure;
+
 class Routes {
 
+	private static array $routes = [];
+
+		public static function get(string $uri, Closure $call) {
+		if($_SERVER['REQUEST_METHOD'] === 'GET') {
+			$uri = trim($uri, '/');
+			return (static::$routes[$uri] = $call);
+		}
+	}
 }
